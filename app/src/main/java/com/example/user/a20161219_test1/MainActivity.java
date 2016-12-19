2ee44b724c5869a1ff4ae78622b229c266182daa
lili <liq1862@gmail.com>
2016-12-19 14:15:23 +0800

    Receiver

--- app/src/main/java/com/example/user/a20161219_test1/MainActivity.java
+++ app/src/main/java/com/example/user/a20161219_test1/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,13 +13,16 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     NotificationManager nm;
+    MyReceiver receiver;
     Intent it;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         it = new Intent(MainActivity.this, MyService.class);
+        receiver = new MyReceiver();
     }
 
     public void clickShow(View v)
@@ -44,5 +48,17 @@ public class MainActivity extends AppCompatActivity {
     public void clickStop(View v)
     {
         stopService(it);
+    }
+
+    public void clickOn(View v)
+    {
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(Intent.ACTION_SCREEN_OFF);
+        registerReceiver(receiver, intentFilter);
+    }
+
+    public void clickOff(View v)
+    {
+        unregisterReceiver(receiver);
     }
 }
---
+++ app/src/main/java/com/example/user/a20161219_test1/MyReceiver.java
@@ -0,0 +1,16 @@
+package com.example.user.a20161219_test1;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class MyReceiver extends BroadcastReceiver {
+    public MyReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d("SCREEN", "Screen off...");
+    }
+}
