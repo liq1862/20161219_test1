9a87207e4ca05c0d3dac454a38c08b384775b8ff
lili <liq1862@gmail.com>
2016-12-19 09:12:07 +0800

    PendingIntent

--- app/src/main/java/com/example/user/a20161219_test1/MainActivity.java
+++ app/src/main/java/com/example/user/a20161219_test1/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.user.a20161219_test1;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,10 +20,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void clickShow(View v){
+        Intent it = new Intent(MainActivity.this,Main2Activity.class);
+        PendingIntent pi = PendingIntent.getActivity(MainActivity.this,123,it,PendingIntent.FLAG_UPDATE_CURRENT);
         Notification.Builder builder = new Notification.Builder(MainActivity.this);
         builder.setContentTitle("標題");
         builder.setContentText("內容");
         builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setContentIntent(pi);
         Notification notification = builder.build();
         nm.notify(1,notification);
     }
